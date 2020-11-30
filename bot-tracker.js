3c0f736531c0a46f0d769e8467a66f66d41940d1
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-11-30 17:38:47 +0300

    launch of the bot

---
+++ bot-tracker.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+const KBot_ID = ""
+const kServer_ID = "777175140647174154"
+
+const client = new Discord.Client()
+
+client.on("ready", () => {
+    console.log("Bot-Tracker is online!")
+})
+
+client.on("presenceUpdate", (oldPresence, newPresence) => {
+    if(newPresence.user.id === KBot_ID) {
+        if(newPresence.server.id === kServer_ID) {
+            if (oldPresence.status !== newPresence.status) {
+                if(newPresence.status === "online") {
+                    client.guild.channels.id.get("508632222245322793").send("<@508632222245322793> Bot is online!")
+                }
+                if(newPresence.status === "offline") {
+                    client.guild.channels.id.get("508632222245322793").send("<@508632222245322793> Bot is offline")
+                }
+            }
+        }
+    }
+});
