76406e4cf318be38d6bef9ddef7dee4517161600
Darkuss420 <64544951+Darkuss420@users.noreply.github.com>
2020-12-07 09:56:11 +0300

    Update bot-tracker.js

--- bot-tracker.js
+++ bot-tracker.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const kBot_ID = "782297343827378216"
 const kServer_ID = "777175140647174154"
+const kLog_Channel_ID = "782960870337544212"
 
 const client = new Discord.Client()
 
@@ -13,26 +14,22 @@ client.on("presenceUpdate", (oldPresence, newPresence) => {
         console.log("Check 2 passed")
     if(newPresence.guild.id === kServer_ID) {
         console.log("Check 3 passed")
-        if (newPresence.status) {
-            newPresence.status.forEach(activity => {
-        if (activity.status === "ONLINE") {
+        if (newPresence.status === "online") {
             console.log(`K-bot is ${kBot_ID.newPresence.status}!`)
             botOnline()
-        } else if(activity.status === "OFFLINE") {
+        } else if(newPresence.status === "offline") {
             console.log(`K-bot is ${kBot_ID.newPresence.status}!`)
             botOffline()
         }
-    });
-   }
-}
+    }
 }
 });
 
 function botOnline() {
-    client.guild.channels.id.get("782960870337544212").send("<@508632222245322793> Bot is online!")
+    client.guild.channels.id.get(kLog_Channel_ID).send("<@508632222245322793> Bot is online!")
 }
 function botOffline() {
-    client.guild.channels.id.get("782960870337544212").send("<@508632222245322793> Bot is offline")
+    client.guild.channels.id.get(kLog_Channel_ID).send("<@508632222245322793> Bot is offline")
 }
 
 client.login(process.env.token)
